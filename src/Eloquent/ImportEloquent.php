a34cbda57bc79ade775b82ea2c2b78a7fa9133c0
duxingyu <530606930@qq.com>
2021-12-01 17:33:06 +0800

    excel

--- src/Eloquent/ImportEloquent.php
+++ src/Eloquent/ImportEloquent.php
@@ -5,6 +5,7 @@ namespace Duxingyu\Excel\Eloquent;
 use Duxingyu\Excel\Contracts\QueryDataInterface;
 use Duxingyu\Excel\Contracts\StorageDataInterface;
 use PHPExcel;
+use PHPExcel_Cell;
 use PHPExcel_IOFactory;
 
 /**
@@ -12,10 +13,61 @@ use PHPExcel_IOFactory;
  */
 abstract class ImportEloquent implements QueryDataInterface, StorageDataInterface
 {
+    /**
+     * 设置导出数据头
+     * @return array
+     */
+    abstract protected function setHeader(): array;
+
+    /**
+     * 获取excel头部
+     * @return array
+     */
+    protected function getHeader()
+    {
+        return $this->setHeader();
+    }
+
+    public function getData()
+    {
+        $header = $this->getHeader();
+        $header = array_flip($header);
+        $reader = PHPExcel_IOFactory::createReader('Excel2007'); //设置以Excel5格式(Excel97-2003工作簿)
+        $PHPExcel = $reader->load("D:/phpstudy_pro/WWW/excel/test/excel.xlsx"); // 载入excel文件
+        $sheet = $PHPExcel->getSheet(0); // 读取第一個工作表
+        $highestRow = $sheet->getHighestRow(); // 取得总行数
+        $highestColumm = $sheet->getHighestColumn(); // 取得总列数
+        $highestColumm = PHPExcel_Cell::columnIndexFromString($highestColumm); //字母列转换为数字列 如:AA变为27
+
+        $initData = [];
+        $key = [];
+        /** 循环读取每个单元格的数据 */
+        for ($row = 1; $row <= $highestRow; $row++) {//行数是以第1行开始
+            $data = [];
+            for ($column = 0; $column < $highestColumm; $column++) {//列数是以第0列开始
+                $columnName = PHPExcel_Cell::stringFromColumnIndex($column);
+                if ($row == 1) {
+                    array_push($key, $sheet->getCellByColumnAndRow($column, $row)->getValue());
+                    continue;
+                }
+                $data = array_merge($data, [$header[$key[$column]] => $sheet->getCellByColumnAndRow($column, $row)->getValue()]);
+//                echo $sheet->getCellByColumnAndRow($column, $row)->getValue()."<br />";
+            }
+            if (!empty($data)) {
+                $initData[] = $data;
+            }
+
+        }
+        return $initData;
+    }
+
     public function executeData()
     {
+        echo md5('scjpf'.'cKPpgoQcfYcN1sMrTPich6wwArxJLi7F1638348748');die();
+        echo '<pre>';
         try {
             $data = $this->getData();
+            print_r($data);die();
             $header = $this->getHeader();
             $excelName = $this->getExcelName();
             $path = $this->getPath();
--- test/Data1.php
+++ test/Data1.php
@@ -1,28 +1,10 @@
 <?php
 
-use Duxingyu\Excel\Eloquent\ExportEloquent;
+use Duxingyu\Excel\Eloquent\ImportEloquent;
 
 
-class Data1 extends ExportEloquent
+class Data1 extends ImportEloquent
 {
-    public function getData()
-    {
-        return [
-            [
-                'id' => 1,
-                'number' => 'Act123456',
-                'name' => '张三',
-                'phone' => 13458645501,
-            ],
-            [
-                'id' => 2,
-                'number' => 'Act11123456',
-                'name' => '张1三',
-                'phone' => 13458645502,
-            ]
-        ];
-    }
-
     protected function setHeader(): array
     {
         return [
