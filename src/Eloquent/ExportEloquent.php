0f28e4cd10f719b28f3157fa333d988ab198d25c
duxingyu <530606930@qq.com>
2021-12-02 13:40:10 +0800

    导出调整

--- src/Eloquent/ExportEloquent.php
+++ src/Eloquent/ExportEloquent.php
@@ -28,9 +28,10 @@ abstract class ExportEloquent implements QueryDataInterface, StorageDataInterfac
             }
             $header = $this->header();
             $path = $this->excelPath();
-            array_unshift($data, $header);
+            if (!empty($header)) {
+                array_unshift($data, $header);
+            }
             $data = array_filter($data);
-            sort($data);
             $objPHPExcel = new PHPExcel();
             $keyIndex = count(reset($data));
             foreach ($data as $key => $value) {
